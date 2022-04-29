1da23bb44dd1996a596f153a4d48ff6171be3e11
Mouiad JRA <mouiad.alilo@gmail.com>
2022-04-29 13:01:57 +0300

    WIP: add say_hello request handler

---
+++ playground/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from playground.views import say_hello
+
+# app_name = 'storefront'
+
+urlpatterns = [
+    path('hello/', say_hello)
+]
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def say_hello(request):
+    return HttpResponse('<H1> Hello world</H1>')

3685bedee8e3eeed943dc95403fc3024b327337d
Mouiad JRA <mouiad.alilo@gmail.com>
2022-04-30 21:45:56 +0300

    WIP: set up the database

--- storefront/settings.py
+++ storefront/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'USER': 'store',
+        'PASSWORD': 'Momohanaj2mf!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 

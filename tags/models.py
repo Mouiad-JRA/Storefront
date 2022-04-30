2cbfdeb1d7473e02cd8d395d7d432f2f44f4f066
Mouiad JRA <mouiad.alilo@gmail.com>
2022-04-30 20:29:30 +0300

    WIP: add TaggedItem and classes.

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,19 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+from store.models import Product
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # what tag is applied to what object (Generic)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type of the Object also the we need the Id of the object
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # watch out if the id is not an integer
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
